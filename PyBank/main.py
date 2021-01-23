33d2b356590be88ff01ba1618c501738d4d02c20
Laura Hertzog <hertzog.laura@gmail.com>
2021-01-23 09:31:31 -0600

    Writes to analysis file for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -29,16 +29,19 @@ with open(budget_data) as csv_file:
         if (profit_loss < greatest_decrease): 
             greatest_decrease = profit_loss
             greatest_decrease_month = month 
-
-    print("Financial Analysis")
-    print("-------------------------")
-    print(f"Total Months: {months}")
-    print(f"Average Change: {net_profit_loss/months}")
-    print(f"Greatest Increase in Profit: {greatest_increase_month} ${greatest_increase}")
-    print(f"Greatest Decrease in Profit: {greatest_decrease_month} ${greatest_decrease}")
     
+def resultsString(): 
+    results = ""
+    results = results + "Financial Analysis\n"
+    results = results + "-------------------------\n"
+    results = results + f"Total Months: {months}\n"
+    results = results + f"Total: {net_profit_loss}\n"
+    results = results + f"Average Change: {net_profit_loss/months}\n"
+    results = results + f"Greatest Increase in Profit: {greatest_increase_month} ${greatest_increase}\n"
+    results = results + f"Greatest Decrease in Profit: {greatest_decrease_month} ${greatest_decrease}\n"
+    return results 
 
-
-
-
-
+results = resultsString()
+print(results)
+with open(os.path.join("analysis", "analysis.txt"),"w") as analysis_file:
+    analysis_file.write(results)
